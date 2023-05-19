8c04d9a129918f287e2c7c8d934bb622bf3f0370
sagar_pant <sagarpant2k2@gmail.com>
2023-05-19 12:01:25 +0530

    resolved image

--- src/ArtesianWells.jsx
+++ src/ArtesianWells.jsx
@@ -36,7 +36,7 @@ While artesian wells offer many advantages, it is crucial to manage them respons
         </p>
         <div className="artesianImg">
 
-        <img src="src/images/artesianWell.jpg" alt="can't load" />
+        <img src="../images/artesianWell.jpg" alt="can't load" />
         </div>
         </div>
 
