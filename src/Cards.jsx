07cb6dcd8536c4b5e01ee915dc07a46272f496d3
sagar_pant <sagarpant2k2@gmail.com>
2023-05-20 22:07:07 +0530

    going Great

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,11 +1,13 @@
 const Card = ({src,info})=>{
         // {console.log("wtf is wrong" + item)}
+    const Data = info.split('\t');
+    console.log(Data)
     return (<div className="Card">
         
         <iframe src={src}></iframe>
 
-        <h2>WellName</h2>
-        <h3>{info}</h3>
+        <h2>{Data[1]}</h2>
+        {/* <h3>{info}</h3> */}
         <button className="button">More</button>
     </div>)
 }
