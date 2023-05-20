4df21909482fbb94c51927a2d143d5acd09b1c54
sagar_pant <sagarpant2k2@gmail.com>
2023-05-20 08:41:26 +0530

    changes

--- src/Card3.jsx
+++ src/Card3.jsx
@@ -1,8 +1,9 @@
-const Card3=({title,src,id=''})=>{
+const Card3=({title,src,id='',desig})=>{
     return (
         <div className="Card3">
         <img src={src} alt="image" />
         <div className="content"><h2>{title}</h2><h3>{id}</h3></div>
+        <div className="content">{desig}</div>
         </div>
     )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -12,7 +12,9 @@ const Home = ()=>{
 We would like to express our sincere gratitude to our esteemed project advisor, Dr. Jyothi Prasad, for their invaluable guidance and support throughout the duration of this project.
             </p>
 <p>
-<Card3 title={"Dr. Jyothi Prasad"} src={"../images/Proff.jpeg"}/>
+    <div className="Container">
+<Card3 title={"Dr. Jyothi Prasad"} src={"../images/Proff.jpeg"} desig={"designation"}/>
+    </div>
     
 Dr. Jyothi Prasad is a distinguished expert in the field of Hydrology-Watershed Management, Irrigation Management and has played a pivotal role in shaping the direction and success of our research. Their extensive knowledge, experience, and passion for the subject matter have been instrumental in enriching our understanding of artesian wells.
 </p>
@@ -34,6 +36,8 @@ Once again, we extend our heartfelt thanks to Dr. Jyothi Prasad for their invalu
 </p>
 
 </p>
+        <div className="Container">
+
         <h1>About Project</h1>
         <p> 
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, dicta numquam officiis ea iste corporis, alias atque dolorem ipsum praesentium, ex quo neque debitis vero autem. Voluptas at rem sed magnam, hic ipsam explicabo saepe provident quia soluta, vitae ratione nemo est expedita tempora doloribus ipsa eos debitis ex excepturi. Atque veritatis fuga perspiciatis autem? Alias aut sapiente blanditiis quisquam autem, eaque architecto itaque sequi at, quae quo? Quibusdam magni unde minima facere ea minus non corrupti dicta commodi officia debitis dolor pariatur quos totam laborum officiis odio impedit suscipit maxime delectus, ad tempore deleniti nostrum iste? Quos, praesentium facilis?
@@ -42,6 +46,7 @@ Once again, we extend our heartfelt thanks to Dr. Jyothi Prasad for their invalu
         <Card2 title={"Artesian Wells"} data={1}/>
         <Card2 title={"Water Streams"} id={2}/>
         <Card2 title={"Rainfall"}  id={3}/>
+        </div>
         </div>
         
     </>)
